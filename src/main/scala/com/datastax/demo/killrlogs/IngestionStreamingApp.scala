1ba2cd91c1b96db60c4e814f8caac55a7f2770da
patoch <pguillebert@gmail.com>
2015-11-08 17:15:07 +0100

    Clean up code, use name tupled.

--- src/main/scala/com/datastax/demo/killrlogs/IngestionStreamingApp.scala
+++ src/main/scala/com/datastax/demo/killrlogs/IngestionStreamingApp.scala
@@ -7,13 +7,12 @@ import org.apache.spark.streaming.kafka.{HasOffsetRanges, OffsetRange, KafkaUtil
 import org.apache.spark.streaming.{Seconds, StreamingContext}
 import org.apache.spark.{SparkConf, SparkContext}
 import com.datastax.spark.connector.streaming._
-import TimeUtils._
+import com.datastax.demo.killrlogs.TimeUtils._
 
 /**
  * Created by Patrick on 19/10/15.
  */
-// dse spark-submit killrlogs-streaming.jar --deploy-mode cluster --supervise
-// dse spark-submit --class com.datastax.demo.killrlogs.IngestionStreamingApp killrlogs-streaming.jar
+// dse spark-submit --deploy-mode cluster --supervise --class com.datastax.demo.killrlogs.IngestionStreamingApp killrlogs-streaming.jar
 
 
 object IngestionStreamingApp extends App {
@@ -68,21 +67,29 @@ object IngestionStreamingApp extends App {
     }
 
     // Aggregate logs in 60 seconds buckets
+    val mapStreamToLogAsTuple: ((String, String)) => (String, String, java.util.Date, java.util.Date, String, String) = {
+      case (k, v) => {
+        val data = v.split(";")
+        // TODO: validate log and append flag for later filtering of malformed logs
+        (data(0), data(1), getMinuteBucketTsFrom(data(2), 60), getMinuteBucketTsFrom(data(2), 1), data(3), data(4))
+      }
+    }
+
     val logs = stream
-      .map(_._2.split(";"))
-      .map( x => (x(0), x(1), getMinuteBucketTsFrom(x(2), 60), getMinuteBucketTsFrom(x(2), 1), x(3), x(4)))
+      .map(mapStreamToLogAsTuple)
       .cache()
 
-    // TODO: filter malformed logs out
-    
+    // TODO: filter invalid logs out and store to invalid_logs table
+
     logs.saveToCassandra("killrlog_ks", "logs", SomeColumns("id", "source_id", "bucket_ts", "ts", "type", "raw"))
 
     val counters = logs // 1 hour bucket, interval 60s
-      .map(x => ((x._2, x._5, getMinuteBucketTsFrom(x._3, 3600)), (1, x._4)))
-      // k(source, type, bucket_ts) v(1, date)
-      .reduceByKey((x, y) => (x._1 + y._1, x._2))
-      // k(source, type, bucket_ts) v(count, date)
-      .map(x => (x._1._1, x._1._2, x._1._3, x._2._2, offsets, x._2._1))
+      .map {case (id, sourceId, bucketTs, ts, logtype, raw) =>
+        ((sourceId, logtype, getMinuteBucketTsFrom(bucketTs, 3600)), (1, ts))}
+      .reduceByKey { case ((reducedCount, reducedTs), (count, ts)) =>
+        (reducedCount + count, reducedTs) }
+      .map {case ((sourceId, logtype, bucketTs),(count, ts)) =>
+        (sourceId, logtype, bucketTs, ts, offsets, count)}
 
     counters.print()
     counters.saveToCassandra("killrlog_ks", "counters", SomeColumns("source_id", "serie_id", "bucket_ts",  "ts", "id", "count"))
